7b20d9869b75eb7938ade6553842b0ca9ef8ead9
Mohanad Elhag <moh.7afiz@gmail.com>
2018-11-29 20:30:43 +0300

    Refactor Project Code

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 
-import Aux from "../../hoc/Aux";
-
+import Aux from "../Aux/Aux";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 import classes from "./Layout.css";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 
 class Layout extends Component {
   state = {
