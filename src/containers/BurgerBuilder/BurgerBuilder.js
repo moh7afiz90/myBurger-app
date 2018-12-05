1e0d75665192d7b5ddddd4afd18097f2709198d2
Mohanad Elhag <moh.7afiz@gmail.com>
2018-12-05 21:06:48 +0300

    Handling Error Copmonent

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,8 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -14,7 +16,7 @@ const INGREDIENT_PRICES = {
   bacon: 0.7
 };
 
-export default class BurgerBuilder extends Component {
+class BurgerBuilder extends Component {
   state = {
     ingredients: {
       salad: 0,
@@ -152,3 +154,5 @@ export default class BurgerBuilder extends Component {
     );
   }
 }
+
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Model from "../../components/UI/Modal/Modal";
+import Aux from "../Aux/Aux";
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    };
+    componentDidMount() {
+      axios.interceptors.request.use(req => {
+        this.setState({ error: null });
+        return req;
+      });
+      axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+    render() {
+      return (
+        <Aux>
+          <Model show={this.state.error} clicked={this.errorConfirmedHandler}>
+            {this.state.error ? this.state.error.message : null}
+          </Model>
+          <WrappedComponent {...this.props} />;
+        </Aux>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
