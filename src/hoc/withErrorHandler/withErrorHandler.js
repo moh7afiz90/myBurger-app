c2f5c0369dc726ea2fedc60265966da53e0e2c3e
Mohanad Elhag <moh.7afiz@gmail.com>
2018-12-05 22:23:41 +0300

    Refactor withErrorHandler Component

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,16 +8,21 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     };
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.requestInterceptors = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.responseInterceptors = axios.interceptors.response.use(
         res => res,
         error => {
           this.setState({ error: error });
         }
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptors);
+      axios.interceptors.response.eject(this.responseInterceptors);
     }
 
     errorConfirmedHandler = () => {
